380ca81c2e34fbc4d4fac4db8948b4d73e222506
CjagEu <48217097+CjagEu@users.noreply.github.com>
2019-03-21 23:31:07 +0100

    Añado la maquina virtual (venv)

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Carlos\Documents\Proyectos Django\bboyweb\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
